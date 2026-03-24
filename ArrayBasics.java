88b32a3200c59ed3ffa8f3e55b951536df50770c
Pradyumna Bhat <122141906+pradyumna100903@users.noreply.github.com>
2026-03-24 21:09:27 +0530

    Add ArrayBasics class with array examples
    Array Basics with initialization

---
+++ ArrayBasics.java
@@ -0,0 +1,34 @@
+package Arrays;
+
+
+
+public class ArrayBasics {
+    public static void main(String[] args) {
+
+
+        // Declaration
+
+        int arr[] ;
+
+        // allocation
+        arr = new int[10];
+
+
+        // Initialization
+
+        int marks[] = {100,90,99,80,887,67,33};
+
+        System.out.println(marks.length);
+
+        System.out.println(" First element in the array present in 0th index is: " + marks[0]);
+        System.out.println(" Second element present in 1st index is: " + marks[1]);
+
+
+
+
+
+
+    }
+
+
+}
