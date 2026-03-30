025682dd55df347397c3e05bd81c135c5fae7f2d
Pradyumna Bhat <122141906+pradyumna100903@users.noreply.github.com>
2026-03-30 13:47:10 +0530

    Implement sum calculation with enhanced for loop
    Added a method to calculate the sum of a 2D array using an enhanced for loop.

--- SumOf2DArrays.java
+++ SumOf2DArrays.java
@@ -9,8 +9,19 @@ public class SumOf2DArrays {
                 {10,20,30}
         };
 
+    // Using Enhanced For Loop or For Each Loop 
+        int forEachSum = 0;
+        for(int[] temp : arr){
+        for(int newTemp : temp) {
+            forEachSum += newTemp;
+        }
+    }    
+
+        
+    System.out.println("The Sum of 2d Array Elements using for each loop is : " +forEachSum);
 
 
+        // Using Normal For Loop 
         int rowSize = arr.length;
         int colSize = arr[0].length;
 
