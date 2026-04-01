57482ed68835ceabec3a28295fd5672bc3a3d58e
Pradyumna Bhat <122141906+pradyumna100903@users.noreply.github.com>
2026-04-01 12:08:53 +0530

    Add First.java with variable declarations and main method
    Meaning and Usage of main keyword ; to demonstrate main is not a reserved keyword in Java

---
+++ First.java
@@ -0,0 +1,18 @@
+package Basics;
+
+public class First {
+
+    public static void main(String[] args) {
+
+
+        // variable initialisation and declaration
+        int age = 29;
+        int AGE = 29;
+        int Age = 29;
+
+        int main= 39;
+
+        System.out.println(main);
+
+    }
+}
