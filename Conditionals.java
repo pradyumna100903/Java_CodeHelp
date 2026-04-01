3d19f4c5551c373d4a85b3c798a3a770c976ae3e
Pradyumna Bhat <122141906+pradyumna100903@users.noreply.github.com>
2026-04-01 12:09:47 +0530

    Implement conditionals in Conditionals.java
    Added a Java program to demonstrate conditional statements including if, if-else, and ternary operator.

---
+++ Conditionals.java
@@ -0,0 +1,46 @@
+package Basics;
+
+import java.util.Scanner;
+
+public class Conditionals {
+
+    public static void main(String[] praddy) {
+
+        Scanner s=new Scanner(System.in);
+        System.out.println("Enter  Your income: ");
+        int income =s.nextInt();
+
+
+      //  if statement
+//
+//        if(income >100000){
+//
+//        }
+
+
+        // if else statement
+
+//        if(){
+//
+//
+//
+//
+//        }
+//
+//        else{
+//
+//
+//
+//        }
+
+        // Ternary Operator Usage
+        System.out.println((income>100000)? "Rich " : "Not Rich");
+
+
+
+
+
+        s.close();
+
+    }
+}
