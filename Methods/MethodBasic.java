8740e1490ddc8779a5b3bbd51970ccb80a64b71b
Pradyumna Bhat <122141906+pradyumna100903@users.noreply.github.com>
2026-04-01 11:47:30 +0530

    Add Func class with main method and callMyName

---
+++ Methods/MethodBasic.java
@@ -0,0 +1,19 @@
+package Methods;
+
+public class Func {
+
+    public static void main(String[] args) {
+        System.out.println(10);
+        callMyName();
+        System.out.println(20);
+
+    }
+
+    public static void callMyName(){
+        System.out.println(1);
+        System.out.println("Praddy");
+        System.out.println(2);
+    }
+
+}
+
