6f3cf5a896ba6035dc13a7a87a840ca8f5673825
Pradyumna Bhat <122141906+pradyumna100903@users.noreply.github.com>
2026-04-04 10:37:38 +0530

    Program to Demonstrate String Creation in Java

---
+++ Strings/String Basics.java
@@ -0,0 +1,26 @@
+package Strings;
+
+public class StringBasics {
+    public static void main(String[] args) {
+
+      // 1st way to declare String Creation using String Literal (Most Recommended and Used way ) 
+        String lastName = "Pradyumna";
+        System.out.println(lastName);
+
+      // 2nd way to declare String Creation using new Keyword .
+        String firstName = new String("Praddy");
+        System.out.println(firstName);
+
+
+        System.out.println(firstName + " " + lastName);
+
+
+        String str = " Babbar " ;
+        str = "Love ";
+
+        System.out.println(str);
+        
+
+
+    }
+}
