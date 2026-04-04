0cf5a4d8f7285caf17a9bd4574c7a5842baa666c
Pradyumna Bhat <122141906+pradyumna100903@users.noreply.github.com>
2026-04-04 20:49:29 +0530

    Enhance comments for string comparison logic
    Added comments to clarify string equality checks in Java.

--- Strings/StringEquality.java
+++ Strings/StringEquality.java
@@ -8,7 +8,7 @@ public class StringEquality {
         String name3 = new String("Praddy");
 
 
- // Here the output will show Strings are not equal because num1 and num3 are pointing to different memory references although their content is same  
+     // Here the output will show Strings are not equal because num1 and num3 are pointing to different memory references although their content is same  
         if(name1 == name3)
         {
             System.out.println("Strings are Equal ");
@@ -17,6 +17,15 @@ public class StringEquality {
             System.out.println("Strings are not equal ");
         }
 
+     // Here the output will show Yes Both are Equal as the content will be compared here .    
+        if(name1.equals(name3))
+        {
+            System.out.println(" Yes Both are Equal ");
+        }
+        else{
+            System.out.println(" No Both are not equal ");
+        }
+        
 
     }
 }
